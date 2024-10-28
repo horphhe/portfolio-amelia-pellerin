a02cb1a26a85ce46a61d58f9357e6ccfe01e2526
horphhe <167013875+horphhe@users.noreply.github.com>
2024-10-28 22:27:39 +0100

    Update script.js

--- pages/danse-dense/script.js
+++ pages/danse-dense/script.js
@@ -4,19 +4,23 @@ const images = document.querySelectorAll('.carousel-images img');
 let currentIndex = 0;
 let interval;
 
+// Fonction pour afficher l'image suivante
 function showNextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     carouselImages.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
+// Démarrer le carousel avec un intervalle
 function startCarousel() {
-    interval = setInterval(showNextImage, 2000); // Changer toutes les 2 secondes
+    interval = setInterval(showNextImage, 3000); // Changer toutes les 3 secondes
 }
 
+// Arrêter le carousel au survol
 function stopCarousel() {
     clearInterval(interval);
 }
 
+// Événements de survol
 carousel.addEventListener('mouseover', stopCarousel);
 carousel.addEventListener('mouseout', startCarousel);
 
